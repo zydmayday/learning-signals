2db8e0226885009f376037821cee5449ac9353e5
zyd <zydmayday@gmail.com>
2023-12-13 22:04:23 +0900

    sample of signals

--- signals/src/App.tsx
+++ signals/src/App.tsx
@@ -1,25 +1,24 @@
-import { useState } from "react";
 import { Box, Button, Grid } from "@radix-ui/themes";
+import { signal } from "@preact/signals-react";
 
+const count = signal(1);
 function App() {
-  const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    console.log("clicked");
+    count.value++;
+    console.log(count);
+  };
 
   return (
     <>
-      <Grid columns="2" gap="3" width="auto">
-        <h1>Vite + React</h1>
+      <Grid columns="1" gap="3" width="auto">
+        <h1>Radix + Signals</h1>
       </Grid>
       <Grid columns="2" gap="3" width="auto">
         <Box className="card">
-          <Button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </Button>
+          <Button onClick={increaseCount}>click</Button>
         </Box>
-        <Box>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </Box>
+        <Box>count is {count.value}</Box>
       </Grid>
     </>
   );
